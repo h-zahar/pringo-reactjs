1b6189cb9d6fadbc4ec582dacdc6a6b1e77b3a7b
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-01-13 18:33:06 +0600

    Filled Section

--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -14,15 +14,15 @@ const Header = () => {
         <header style={{position: 'sticky', top: '0', zIndex: '4'}} className="bg-dark">
             <div>
                 <nav style={{color: 'white'}} className="d-flex justify-content-center">
-                    <ul className="nav-style d-flex align-items-center my-auto">
+                    <ul className="nav-style d-flex flex-wrap justify-content-center align-items-center my-auto">
                         <li style={{marginLeft: '-23px'}}><NavLink activeStyle={activeStyle} to="/feed">Feed</NavLink></li>
                         <li><NavLink activeStyle={activeStyle} to="/pringo">Pringo!</NavLink></li>
                         <li><NavLink activeStyle={activeStyle} to="/prings">Prings</NavLink></li>
+                        <li><NavLink activeStyle={activeStyle} to="/profile">Profile</NavLink></li>
                         {
                             user?.email &&
                             <li className="text-success">{user?.displayName}</li>
                         }
-                        <li><NavLink activeStyle={activeStyle} to="/profile">Profile</NavLink></li>
                         <li className="nav-item">
                             <button onClick={logOut} className="btn btn-outline-light">Logout</button>
                         </li>
