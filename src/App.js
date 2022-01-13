2a3afeeb0229d522d6aa7352d1731b8ca08ec40a
Hassan Zahar Rifat <64719808+iFall-iFail@users.noreply.github.com>
2022-01-13 10:52:51 +0600

    Set up Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
 import './App.css';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
+import AuthProvider from './contexts/AuthProvider';
+import initializeAuthentication from './firebase/firebase.init';
+import Login from './pages/Home/Login/Login';
+import Register from './pages/Home/Register/Register';
+import InsidePringo from './pages/InsidePringo/InsidePringo/InsidePringo';
+
+
+initializeAuthentication();
 
 function App() {
   return (
     <>
+      <AuthProvider>
+        <Router>
+          <Switch>
+            <Route exact path='/'>
+              <Login />
+            </Route>
 
+            <Route path='/register'>
+              <Register />
+            </Route>
+
+            <Route path='/feed'>
+              <InsidePringo />
+            </Route>
+          </Switch>
+        </Router>
+      </AuthProvider>
     </>
   );
 }
---
+++ src/pages/InsidePringo/InsidePringo/InsidePringo.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Feed from '../Feed/Feed';
+
+const InsidePringo = () => {
+    return (
+        <div>
+            <Feed />
+        </div>
+    )
+}
+
+export default InsidePringo;
